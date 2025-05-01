3d6db281eadf5d6b93db8fec5f774a7877b6866b
Pankaj7277 <pankajkr.v21@gmail.com>
2025-05-01 14:58:55 +0530

    updated code

--- lib/main.dart
+++ lib/main.dart
@@ -49,9 +49,6 @@ Future<Position> _determinePosition() async {
   // Test if location services are enabled.
   serviceEnabled = await Geolocator.isLocationServiceEnabled();
   if (!serviceEnabled) {
-    // Location services are not enabled don't continue
-    // accessing the position and request users of the
-    // App to enable the location services.
     return Future.error('Location services are disabled.');
   }
 
@@ -62,13 +59,11 @@ Future<Position> _determinePosition() async {
       return Future.error('Location permissions are denied');
     }
   }
-
   if (permission == LocationPermission.deniedForever) {
     // Permissions are denied forever, handle appropriately.
     return Future.error(
         'Location permissions are permanently denied, we cannot request permissions.');
   }
-
   // When we reach here, permissions are granted and we can
   // continue accessing the position of the device.
   return await Geolocator.getCurrentPosition();
